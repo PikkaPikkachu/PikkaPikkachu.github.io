8571cfb82fab13bdea6fbdae72010912b938b830
PikkaPikkachu <prakriti_bansal@yahoo.com>
2023-02-20 00:28:10 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# PikkaPikkachu.github.io
--- src/context/context.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const PortfolioContext = React.createContext();
-PortfolioContext.name = "Prakriti";
-export const PortfolioProvider = PortfolioContext.Provider;
-export const PortfolioConsumer = PortfolioContext.Consumer;
-
-export default PortfolioContext;
